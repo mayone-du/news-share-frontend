a4f5724e6545ff3c31a0ccd64f8acf040ba4f8ad
mayone-du <cocomayo1201@icloud.com>
2021-06-28 19:08:51 +0900

    Edit API_ENDPOINTS

--- src/utils/API_ENDPOINTS.ts
+++ src/utils/API_ENDPOINTS.ts
@@ -1,6 +1,6 @@
 export const GRAPHQL_API_ENDPOINT =
   process.env.NODE_ENV === "development"
-    ? `${process.env.NEXT_PUBLIC_DEV_GRAPHQL_API_URL}graphql/`
+    ? `${process.env.NEXT_PUBLIC_DEV_GRAPHQL_API_URL}`
     : `${process.env.GRAPHQL_API_URL}`;
 
 export const MEDIAFILE_API_ENDPOINT =

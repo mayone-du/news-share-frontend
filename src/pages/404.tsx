8699a3099ee9c5b25df0137b02d157755fff80c1
mayone-du <cocomayo1201@icloud.com>
2021-06-29 19:45:21 +0900

    Add 404 props

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import { Layout } from "src/components/layouts/Layout";
 const NotFoundPage = () => {
   return (
     <div>
-      <Layout metaTitle="404 Not Found">
+      <Layout metaTitle="404 Not Found" currentPagePath="/404">
         <h2 className="text-3xl">404 Not Found</h2>
       </Layout>
     </div>

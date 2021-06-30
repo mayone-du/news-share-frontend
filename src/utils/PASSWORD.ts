e361d85d912cf4bb73e1e96f87f1f6bd5c25c140
mayone-du <cocomayo1201@icloud.com>
2021-06-30 19:30:03 +0900

    Fix password

--- src/utils/PASSWORD.ts
+++ src/utils/PASSWORD.ts
@@ -1,4 +1,4 @@
 export const SLACK_PASSWORD =
   process.env.NODE_ENV === "development"
     ? `${process.env.NEXT_PUBLIC_SLACK_PASSWORD}`
-    : `${process.env.SLACK_API_PASSWORD}`;
+    : `${process.env.SLACK_PASSWORD}`;
